678b5540187e486a7d0e4abff00c85d2e7509153
John Otander <johnotander@gmail.com>
2015-01-21 11:50:11 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var cssAbsoluteUnits = require('..');
+
+describe('css-absolute-units', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(cssAbsoluteUnits(), true);
+  });
+});
